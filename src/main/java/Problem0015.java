22dcded70deb33b06f7e667dedea02ad788de0b9
David Clutter <david.clutter@ca.com>
2015-09-23 16:38:38 -0600

    Add Problem0015

---
+++ src/main/java/Problem0015.java
@@ -0,0 +1,27 @@
+/**
+ * Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+ * <p>
+ * How many such routes are there through a 20×20 grid?
+ *
+ * @author david.clutter@ca.com
+ */
+public class Problem0015 {
+
+    private static final int GRID_SIZE = 20;
+
+    // Dynamic Backtrack
+    public static void main(final String[] args) {
+        final long[][] grid = new long[GRID_SIZE + 1][GRID_SIZE + 1];
+        for (int i = GRID_SIZE; i >= 0; --i) {
+            for (int j = GRID_SIZE; j >= 0; --j) {
+                if (i == GRID_SIZE || j == GRID_SIZE) {
+                    grid[i][j] = 1;
+                } else {
+                    grid[i][j] = grid[i + 1][j] + grid[i][j + 1];
+                }
+            }
+        }
+        grid[GRID_SIZE][GRID_SIZE] = 0;
+        System.out.print(grid[0][0]);
+    }
+}
