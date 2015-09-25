9339479423d1657d24a4ece45276923b0f688404
David Clutter <cluttered.code@gmail.com>
2015-09-24 22:07:33 -0600

    fix class javadoc

--- src/main/java/Problem0015.java
+++ src/main/java/Problem0015.java
@@ -1,9 +1,10 @@
 /**
- * Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+ * Starting in the top left corner of a 2x2 grid, and only being able to move to the right and down,
+ * there are exactly 6 routes to the bottom right corner.
  * <p>
- * How many such routes are there through a 20×20 grid?
+ * How many such routes are there through a 20x20 grid?
  *
- * @author david.clutter@ca.com
+ * @author David Clutter
  */
 public class Problem0015 {
 
